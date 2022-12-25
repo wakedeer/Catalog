0df5b21cd6ccde266843de7640c0201146a50cfc
Aleksei Mironov <aumironov@vtb.ru>
2022-12-25 11:46:21 +0300

    add main structure of product server

---
+++ product-service/src/main/java/inno/tech/ProductApp.java
@@ -0,0 +1,13 @@
+package inno.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApp.class, args);
+    }
+
+}
---
+++ product-service/src/main/java/inno/tech/api/ProductApiController.java
@@ -0,0 +1,21 @@
+package inno.tech.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductApiController {
+
+    private final OAuth2RestTemplate oAuth2RestTemplate;
+
+    @GetMapping("/product/{product_id}")
+    public ResponseEntity<String> getProductInfo(@PathVariable("product_id") String productId) {
+        //TODO
+        return ResponseEntity.ok("OK with " + productId);
+    }
+}
