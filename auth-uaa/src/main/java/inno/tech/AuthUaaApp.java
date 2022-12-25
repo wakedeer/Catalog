1406b4989a906933ee2768f9542ca3e58bc4004c
Aleksei Mironov <aumironov@vtb.ru>
2022-12-25 11:17:46 +0300

    add main structure of auth server

---
+++ auth-uaa/src/main/java/inno/tech/AuthUaaApp.java
@@ -0,0 +1,12 @@
+package inno.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthUaaApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthUaaApp.class, args);
+    }
+}
---
+++ auth-uaa/src/main/java/inno/tech/config/JwkAuthorizationServerConfiguration.java
@@ -0,0 +1,55 @@
+package inno.tech.config;
+
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.KeyUse;
+import com.nimbusds.jose.jwk.RSAKey;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+
+import java.security.KeyPair;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Collections;
+import java.util.Map;
+
+@Configuration
+@EnableAuthorizationServer
+public class JwkAuthorizationServerConfiguration {
+
+    private static final String KEY_STORE_FILE = "amironov-jwt.jks";
+    private static final String KEY_STORE_PASSWORD = "amironov-pass";
+    private static final String KEY_ALIAS = "amironov-oauth-jwt";
+    private static final String JWK_KID = "amironov-key-id";
+
+    @Bean
+    public TokenStore tokenStore(JwtAccessTokenConverter jwtAccessTokenConverter) {
+        return new JwtTokenStore(jwtAccessTokenConverter);
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        Map<String, String> customHeaders = Collections.singletonMap("kid", JWK_KID);
+        return new JwtCustomHeadersAccessTokenConverter(customHeaders, keyPair());
+    }
+
+    @Bean
+    public KeyPair keyPair() {
+        ClassPathResource ksFile = new ClassPathResource(KEY_STORE_FILE);
+        KeyStoreKeyFactory ksFactory = new KeyStoreKeyFactory(ksFile, KEY_STORE_PASSWORD.toCharArray());
+        return ksFactory.getKeyPair(KEY_ALIAS);
+    }
+
+    @Bean
+    public JWKSet jwkSet() {
+        RSAKey.Builder builder = new RSAKey.Builder((RSAPublicKey) keyPair().getPublic()).keyUse(KeyUse.SIGNATURE)
+                .algorithm(JWSAlgorithm.RS256)
+                .keyID(JWK_KID);
+        return new JWKSet(builder.build());
+    }
+}
